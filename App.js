71074371c6775822dd02419e43bc03a53aae14fb
MiPeSa <bgu109@myy.haaga-helia.fi>
2023-01-22 18:16:01 +0200

    Task done

--- App.js
+++ App.js
@@ -1,11 +1,54 @@
+import React from'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, Alert, TextInput } from 'react-native';
 
 export default function App() {
+
+  const [number, setNumber] = React.useState('');
+  const [secNumber, setSecNumber] = React.useState('');
+  const [answer, setAnswer] = React.useState('');
+  const [open, setOpen] = React.useState(false);
+
+  const buttonPressed = (button) => {
+    if ((button) === '+') {
+      setAnswer(parseInt(number) + parseInt(secNumber));
+      setOpen(true);
+    }
+    else if (button === '-') {
+      setAnswer(parseInt(number) - parseInt(secNumber));
+      setOpen(true);
+    } 
+    else{
+      Alert.alert('error')
+    }
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.headerBox}>
+          <Text style={styles.header}>Simple Calculator</Text>
+          <Text>Set numbers and press + or -</Text>
+      </View>
+          <Text style={styles.results} open={false}>Result: {answer}</Text>
       <StatusBar style="auto" />
+      <View>
+          <TextInput
+              style={styles.textinput}
+              keyboardType = 'numeric'
+              onChangeText={number => setNumber(number)}
+              value={number}
+          />
+          <TextInput
+              style={styles.textinput}
+              keyboardType = 'numeric'
+              onChangeText={secNumber => setSecNumber(secNumber)}
+              value={secNumber}
+          />
+      </View>
+      <View style={styles.button}>
+          <Button color="lightgreen" width="50" onPress={() => buttonPressed('+')} title={'+'} />
+          <Button color="red" onPress={() => buttonPressed('-')} title={'-'} />  
+      </View>
     </View>
   );
 }
@@ -13,8 +56,29 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'white',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  button: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    margin: 10,
+    width: "25%",
+  },
+  textinput: {
+    width: 150,
+    height: 30,
+    borderColor: 'black',
+    borderWidth: 1,
+  },
+  header: {
+    fontSize: 25
+  },
+  results: {
+    fontSize: 18
+  },
+  headerBox: {
+    margin: 20,
+  }
 });
